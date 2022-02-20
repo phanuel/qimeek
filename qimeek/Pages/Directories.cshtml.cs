cb43a6645a9dd23dd70e52530fb49cfb7c9a097a
phanuel <phanuel@stillnorth.net>
2022-02-20 13:46:06 +0100

    add search

--- qimeek/Pages/Directories.cshtml.cs
+++ qimeek/Pages/Directories.cshtml.cs
@@ -35,28 +35,48 @@ namespace qimeek.Pages
         public Directory CurrentDirectory { get; set; }
         public List<Directory> ParentDirectories { get; set; }
         public List<Directory> SubDirectories { get; set; }
+        public string Query { get; set; }
         public string ThumbnailProviderLink { get; set; }
 
-        public void OnGet(int id)
+        public void OnGet(int id, string query)
         {
             string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
             // get all directories of the user, not only the subdirectories of the current directory because we need all dirs to build the breadcrumb (without making multiple db trips)
             List<Directory> directories = _dbContext.Directories.Where(d => d.UserId == userId).Select(x => new Directory { Id = x.Id, Name = x.Name, ParentId = x.ParentId, Bookmarks = x.Bookmarks }).ToList();
 
-            CurrentDirectory = directories.Where(d => d.Id == id).First();
-            SubDirectories = directories.Where(d => d.ParentId == CurrentDirectory.Id).ToList();
-
-            // find parent directories for the breadcrumb
-            Directory parentDir = CurrentDirectory;
-            ParentDirectories = new List<Directory>();
-            while(parentDir.ParentId != null)
+            if (string.IsNullOrEmpty(query))
             {
-                parentDir = directories.Where(d => d.Id == parentDir.ParentId).First();
-                ParentDirectories.Add(parentDir);
+                CurrentDirectory = directories.Where(d => d.Id == id).First();
+                SubDirectories = directories.Where(d => d.ParentId == CurrentDirectory.Id).ToList();
+
+                // find parent directories for the breadcrumb
+                Directory parentDir = CurrentDirectory;
+                ParentDirectories = new List<Directory>();
+                while (parentDir.ParentId != null)
+                {
+                    parentDir = directories.Where(d => d.Id == parentDir.ParentId).First();
+                    ParentDirectories.Add(parentDir);
+                }
+                // put parentDirs in the right order
+                ParentDirectories.Reverse();
             }
-            // put parentDirs in the right order
-            ParentDirectories.Reverse();
+            else
+            {
+                CurrentDirectory = new Directory() { Name = "Search" };
+
+                foreach (Directory directory in directories)
+                {
+                    var matches = directory.Bookmarks.Where(b => b.Title.ToLower().Contains(query.ToLower())).ToList();
+
+                    foreach (Bookmark bookmark in matches)
+                    {
+                        CurrentDirectory.Bookmarks.Add(bookmark);
+                    }
+                }
+                
+                SubDirectories = directories.Where(d => d.Name.Contains(query)).ToList();
+            }
 
             Uri uri = new Uri(Config.ThumbnailProviderUrl);
             ThumbnailProviderLink = $"{uri.Scheme}://{uri.Host}";
